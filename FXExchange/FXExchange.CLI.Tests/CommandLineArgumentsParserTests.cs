fb24a40fe3d8567bcc194e4223898b4af4d076dd
Andrius Razinskas <andrius.razinskas@juvare.com>
2025-01-11 19:29:49 +0200

    Added CommandLineArgumentsParser tests, tests written before actual implementation to prove that TDD was used

---
+++ FXExchange/FXExchange.CLI.Tests/CommandLineArgumentsParserTests.cs
@@ -0,0 +1,97 @@
+﻿using FXExchange.Core;
+
+namespace FXExchange.CLI.Tests;
+
+[TestFixture]
+internal class CommandLineArgumentsParserTests
+{
+    private CommandLineArgumentsParser _parser;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _parser = new CommandLineArgumentsParser();
+    }
+
+    [TestCase(new[] { "EUR/USD", "1" }, Currency.EUR)]
+    [TestCase(new[] { "usd/gbp", "2" }, Currency.USD)]
+    [TestCase(new[] { " gbp / sek ", "3" }, Currency.GBP)]
+    public void Parse_ParsesMainCurrency_WhenInputIsValid(string[] args, Currency expectedCurrency)
+    {
+        var request = _parser.Parse(args);
+
+        Assert.That(request.MainCurrency, Is.EqualTo(expectedCurrency));
+    }
+
+    [TestCase(new[] { "EUR/USD", "1" }, Currency.USD)]
+    [TestCase(new[] { "usd/gbp", "2" }, Currency.GBP)]
+    [TestCase(new[] { " gbp / sek ", "3" }, Currency.SEK)]
+    public void Parse_ParsesTargetCurrency_WhenInputIsValid(string[] args, Currency expectedCurrency)
+    {
+        var request = _parser.Parse(args);
+
+        Assert.That(request.TargetCurrency, Is.EqualTo(expectedCurrency));
+    }
+
+    [TestCase(new[] { "EUR/USD", "1.12345" }, 1.12345)]
+    [TestCase(new[] { "EUR/USD", "1,12345" }, 1.12345)]
+    [TestCase(new[] { "USD/GBP", "2" }, 2)]
+    public void Parse_ParsesAmountToExchange_WhenInputIsValid(string[] args, decimal expectedAmount)
+    {
+        var request = _parser.Parse(args);
+
+        Assert.That(request.AmountToExchange, Is.EqualTo(expectedAmount));
+    }
+
+    [TestCase(0)]
+    [TestCase(1)]
+    [TestCase(3)]
+    public void Parse_ThrowsException_WhenIncorrectNumberOfArgumentsAreProvided(int argumentsCount)
+    {
+        var arguments = Enumerable.Range(0, argumentsCount).Select(i => i.ToString()).ToArray();
+
+        var exception = Assert.Throws<Exception>(() => _parser.Parse(arguments));
+
+        Assert.That(exception.Message, Is.EqualTo("Incorrect arguments count. Usage: FXExchange.CLI.exe <currency pair> <amount to exchange>"));
+    }
+
+    [Test]
+    public void Parse_ThrowsException_WhenMainCurrencyIsIncorrect()
+    {
+        var arguments = new [] { "EUG/USD", "1.12345" };
+
+        var exception = Assert.Throws<Exception>(() => _parser.Parse(arguments));
+
+        Assert.That(exception.Message, Is.EqualTo("Main currency could not be parsed."));
+    }
+
+    [Test]
+    public void Parse_ThrowsException_WhenTargetCurrencyIsIncorrect()
+    {
+        var arguments = new[] { "EUR/UGD", "1.12345" };
+
+        var exception = Assert.Throws<Exception>(() => _parser.Parse(arguments));
+
+        Assert.That(exception.Message, Is.EqualTo("Target currency cannot be parsed."));
+    }
+
+    [Test]
+    public void Parse_ThrowsException_WhenAmountToExchangeIsIncorrect()
+    {
+        var arguments = new[] { "EUR/USD", "1abc" };
+
+        var exception = Assert.Throws<Exception>(() => _parser.Parse(arguments));
+
+        Assert.That(exception.Message, Is.EqualTo("Amount to exchange cannot be parsed."));
+    }
+
+    [Test]
+    public void Parse_ThrowsException_WhenAmountToExchangeIsNegative()
+    {
+        var arguments = new[] { "EUR/USD", "-1.2" };
+
+        var exception = Assert.Throws<Exception>(() => _parser.Parse(arguments));
+
+        Assert.That(exception.Message, Is.EqualTo("Amount to exchange cannot be negative."));
+    }
+}
---
+++ FXExchange/FXExchange.CLI/CommandLineArgumentsParser.cs
@@ -0,0 +1,14 @@
+﻿namespace FXExchange.CLI;
+
+internal interface ICommandLineArgumentsParser
+{
+    CurrencyExchangeRequest Parse(string[] arguments);
+}
+
+internal class CommandLineArgumentsParser : ICommandLineArgumentsParser
+{
+    public CurrencyExchangeRequest Parse(string[] args)
+    {
+        throw new NotImplementedException();
+    }
+}
